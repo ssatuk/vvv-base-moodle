7d070e5bb3a94fe6b9ef6d23a76a2120f5c90665
Julian Elve <julian.elve@gmail.com>
2019-04-23 12:41:23 +0100

    Go back to previous with submodules

--- src/Provisioner.php
+++ src/Provisioner.php
@@ -133,7 +133,7 @@ class Provisioner
 
         $this->getCmdWithWD(
             $this->base_dir,
-            array('git', 'submodule', 'init'),
+            array('git', 'submodule', 'update', '--init', '--recursive'),
             array(),
             900
         )->mustRun()->getOutput();
